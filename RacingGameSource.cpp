a2f5b904a8a85afbc39a6b7b2ae9c5bf14f38a93
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-07-11 08:16:19 +0300

    Update RacingGameSource.cpp

--- RacingGameSource.cpp
+++ RacingGameSource.cpp
@@ -1,42 +1,24 @@
-﻿#include <iostream>
 #include <time.h>
 #include "ClassCar.h"
-#include "ClassRout.h"
-#include "EnumDirection.h"
+#include "ClassRoad.h"
 #include "CarFunctions.h"
-#include "RoutFunctions.h"
-#include "DescribeDirection.h"
+#include "RoadFunctions.h"
+#include "GameMovement.h"
 
 using namespace std;
-
-void clearscreen()
-{
-	HANDLE hOut;
-	COORD Position;
-
-	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	Position.X = 0;
-	Position.Y = 0;
-	SetConsoleCursorPosition(hOut, Position);
-}
 
 int	main()
 {
 	srand(time(0));
-	Route road;
-	int a;
 	while (true)
 	{
-		GetKeyboardKeys();
-		DiscribePlayerDirectionProperties();
-		DiscribeEnemyDirectionProperties();
-		enemyCar.RespawningEnemyCar();
-		road.TouchingTheWall();
-		road.BuildingRout();
-		road.Rules();
-		cout << &a << endl;
-		//system("cls");
+		playerCar.GetKeyboardKeys();
+		playerCar.Movement(playerCarDirection);
+		playerCar.TouchTheWall();
+		playerCar.CarCrash();
+		enemyCar.Movement(enemyCarDirection);
+		enemyCar.RespawningCar();
+		aRoad.BuildRoad();
 		Sleep(100);
 		clearscreen();
 	}
