a747d5c8b305e979873cd921f1509fa0eb896660
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-07-11 08:15:55 +0300

    Update CarFunctions.h

--- CarFunctions.h
+++ CarFunctions.h
@@ -1,51 +1,67 @@
 #pragma once
 #ifndef CarFunctions
 #define CarFunctions
-#include <iostream>
 #include <stdlib.h>
 #include "ClassCar.h"
 
 using namespace std;
 
-void Car::BuildingCarPart1()
+void Car::BuildCar(int color)
 {
-	cout << " " << sign << " ";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+	cout << sign;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
 
-void Car::BuildingCarPart2()
+void Car::CarGetCoordinates()
 {
-	cout << sign << sign << sign;
+	x[1] = x[0] + 1;
+	x[2] = x[0] + 1;
+	x[3] = x[0] + 1;
+	x[4] = x[0] + 2;
+	x[5] = x[0] + 3;
+	x[6] = x[0] + 4;
+	x[7] = x[0] + 4;
+	x[8] = x[0] + 4;
+
+	y[1] = y[0] - 1;
+	y[2] = y[0] + 1;
+	y[3] = y[0];
+	y[4] = y[0];
+	y[5] = y[0];
+	y[6] = y[0] - 1;
+	y[7] = y[0];
+	y[8] = y[0] + 1;
 }
 
-void Car::SpawningEnemyCar()
+void EnemyCar::SpawningCar()
 {
-	enemyCarY = rand() % 7;
+	y[0] = rand() % 8 + 2;
 
-	if (enemyCarY != 0 || enemyCarY != 6)
+	if (y[0] != 2 || y[0] != 7)
 	{
 		int leftOrRight = rand() % 2; //chance 50% enemy car to be in rigth or left side of the route
 
 		if (leftOrRight == 0)
 		{
-			enemyCarY = 1;
+			y[0] = 3;
+			CarGetCoordinates();
 		}
 		else
 		{
-			enemyCarY = 6;
+			y[0] = 7;
+			CarGetCoordinates();
 		}
 	}
 }
 
-void Car::RespawningEnemyCar()
+void EnemyCar::RespawningCar()
 {
-	if (enemyCarX == 20)
+	if (x[0] == 20)
 	{
-		SpawningEnemyCar();
-		enemyCarX = 1;
-		enemyCarX1 = 2;
-		enemyCarX2 = 3;
-		enemyCarX3 = 4;
-		enemyCarX4 = 5;
+		x[0] = 2;
+		score++;
+		SpawningCar();
 	}
 }
 
